5d02d512a6571beeaf6997123e4d8ab1baa3a891
Erich Hohlman <EWH@Erich-Hohlmans-MacBook-Pro.local>
2017-09-24 19:38:37 -0400

    Creation of dictionaries and key validation functions

---
+++ weddings.py
@@ -0,0 +1,54 @@
+#create a program that will calculate multiple variations of wedding venue costs (price per person, minimums)
+#list of venues, list of photographers, list of florists, list of djs
+
+venues = {
+        'Ryland Inn' = 33000,
+        'Falkirk' = 27000,
+        'The Old Field Club' = 25000,
+        'Perona Farms' = 32000,
+        'The Whitby' = 28000
+    }
+
+djs = {
+    'Scratch Weddings' = 1500,
+    'The DJ' = 2000,
+    'Hank Lane' = 2500
+}
+
+photographers = {
+    'Jen Larsen' = 4900,
+    'Bri Johnson' = 5100,
+    'Debbie' = 4800,
+    'Lauren' = 5000
+}
+
+florist = {
+    'Florist1' = 4500,
+    'Florist2' = 4000,
+    'Florist3' = 3500
+}
+
+def validVenues(venue):
+    if venue in venues:
+        return True
+    else:
+        return False
+
+def validDjs(dj):
+    if dj in djs:
+        return True
+    else:
+        return False
+
+def validPhotographers(photographer):
+    if photographer in photographers:
+        return True
+    else:
+        return False
+
+def validFlorists(florist):
+    if florist in florists:
+        return True
+    else:
+        return False
+
