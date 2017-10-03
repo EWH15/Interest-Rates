f59e66e10f9b4bbe4cad51dc56d404ff62054c71
Erich Hohlman <EWH@Erich-Hohlmans-MacBook-Pro.local>
2017-10-02 21:32:39 -0400

    Added convertCurrency function test

---
+++ currency.test.py
@@ -0,0 +1,21 @@
+import currency
+import unittest
+
+class CurrencyTest(unittest.TestCase):
+    def test_validCurrency(self):
+        self.assertEqual(currency.isValidCurrency('USD'), True)
+        self.assertEqual(currency.isValidCurrency('GBP'), True)
+        self.assertEqual(currency.isValidCurrency('EURO'), True)
+        self.assertEqual(currency.isValidCurrency('AUD'), True)
+
+    def test_invalidCurrency(self):
+        self.assertEqual(currency.isValidCurrency('JPY'), False)
+
+    def test_convertCurrency(self):
+        self.assertEqual(currency.convertCurrency('USD','EURO', 100), (1/1.176)*100)
+    
+    def test_falseConvertCurrency(self):
+        self.assertEqual(currency.convertCurrency('JPY', 'CHF', 100), False)
+
+if __name__ == '__main__':
+    unittest.main()
